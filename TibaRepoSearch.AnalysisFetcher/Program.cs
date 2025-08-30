2103a6e83bb0ff19495d303dab0b43075bbdef79
Guy Signer <signer@mail.com>
2025-08-31 00:30:16 +0300

    analysis fetcher deps fix

--- TibaRepoSearch.AnalysisFetcher/Program.cs
+++ TibaRepoSearch.AnalysisFetcher/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TibaRepoSearch;
@@ -24,6 +25,20 @@ var host = Host.CreateDefaultBuilder(args)
         
         services.AddSingleton<IEventPublisher<RepositoryAnalysisReadyMessage>>(provider =>
             new RabbitMQEventPublisher<RepositoryAnalysisReadyMessage>(rabbitMqHost, analysisReadyQueue, provider.GetRequiredService<ILogger<RabbitMQEventPublisher<RepositoryAnalysisReadyMessage>>>(), rabbitMqUser, rabbitMqPassword));
+        services.Configure<GithubClientOptions>(options => { options.RetryCount = 3; options.DelaySeconds = 1; });
+        services.AddHttpClient<IGithubClient, GitHubClient>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.github.com/");
+            client.DefaultRequestHeaders.Add("User-Agent", "TibaRepoSearch");
+        });
+        
+        var dbHost = context.Configuration["Database:Host"] ?? "localhost";
+        var dbName = context.Configuration["Database:Name"] ?? "tibarepodb";
+        var dbUser = context.Configuration["Database:User"] ?? "tibauser";
+        var dbPassword = context.Configuration["Database:Password"] ?? "tibapass";
+        var connectionString = $"Host={dbHost};Database={dbName};Username={dbUser};Password={dbPassword}";
+        services.AddDbContextFactory<FavoriteRepositoriesContext>(options => options.UseNpgsql(connectionString));
+        
         services.AddSingleton<IFetchRepositoryAnalysisUseCase, FetchRepositoryAnalysisUseCase>();
         services.AddSingleton<IAddOrUpdateFavoriteRepositoryAnalysisCommandFactory, AddOrUpdateFavoriteRepositoryAnalysisCommandFactory>();
         services.AddSingleton<IRepositoryFavoritedEventHandler, RepositoryFavoritedEventHandler>();
